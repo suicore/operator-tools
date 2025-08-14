3914ea36d835f024c4af53ed15ceaf14396d165e
Dimitris Frangiadakis <dimitris@frangiadakis.com>
2025-08-14 11:04:50 +0300

    Upgraded WAL package

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // MAINNET
-export const WALRUS_PKG = "0xfdc88f7d7cf30afab2f82e8380d11ee8f70efb90e863d1de8616fae1bb09ea77";
+export const WALRUS_PKG = "0xfa65cb2d62f4d39e60346fb7d501c12538ca2bbc646eaa37ece2aec5f897814e";
 export const STAKING_OBJ = "0x10b9d30c28448939ce6c4d6c6e0ffce4a7f8a4ada8248bdad09ef8b70e4a3904";
 export const WALRUS_INNER_STAKING_OBJ = "0x2134d52768ea07e8c43570ef975eb3e4c27a39fa6396bef985b5abc58d03ddd2";
 
